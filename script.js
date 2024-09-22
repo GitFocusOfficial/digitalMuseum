2116a0a92d503d02ec815ab3a08b2bd4e4b34755
GitFocus <43635097+GitFocusOfficial@users.noreply.github.com>
2024-09-22 11:17:20 +0800

    Added code to index.html styles.css script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+// Functionality to open and close modal when clicking on images
+document.querySelectorAll('.photo img').forEach(image => {
+    image.addEventListener('click', function() {
+        // Create modal element
+        const modal = document.createElement('div');
+        modal.classList.add('modal');
+        
+        // Add image and close button to modal
+        modal.innerHTML = `
+            <div class="modal-content">
+                <img src="${this.src}" alt="${this.alt}">
+                <span class="close-modal">&times;</span>
+            </div>
+        `;
+
+        // Append modal to body
+        document.body.appendChild(modal);
+        
+        // Close modal when clicking the close button
+        modal.querySelector('.close-modal').addEventListener('click', function() {
+            modal.remove();
+        });
+
+        // Close modal when clicking outside the image
+        modal.addEventListener('click', function(event) {
+            if (event.target === modal) {
+                modal.remove();
+            }
+        });
+    });
+});
